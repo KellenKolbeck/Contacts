619cd2244864a924ad203b4961cc7c8f962ebf42
Andrew Haupt & Kellen Kolbeck <student@epicodus.com>
2015-08-13 10:00:54 -0700

    Add getter methods for job and company

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,13 +1,15 @@
 class Contacts
 
-  attr_reader(:full_name, :first_name, :last_name)
+  attr_reader(:full_name, :first_name, :last_name, :job, :company)
 
   define_method(:initialize) do |attributes|
-    @full_name = attributes.fetch(:full_name)
+    @full_name = attributes.fetch(:full_name).split.each{|word| word.capitalize!}.join(" ")
     @first_name = @full_name.split
     @first_name = @first_name[0]
     @last_name = @full_name.split
     @last_name = @last_name[1]
+    @job = attributes.fetch(:job).split.each{|word| word.capitalize!}.join(" ")
+    @company = attributes.fetch(:company).split.each{|word| word.capitalize!}.join(" ")
   end
 
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -9,22 +9,36 @@ require('pry')
 describe(Contacts) do
   describe('#full_name') do
     it("displays the contacts full name") do
-      test_name = Contacts.new({:full_name => "Ricky Bobby"})
+      test_name = Contacts.new({:full_name => "Ricky Bobby", :job => "race car driver", :company => "Wonderbread racing"})
       expect(test_name.full_name()).to(eq("Ricky Bobby"))
     end
   end
 
   describe('#first_name') do
     it("displays the contacts first name") do
-      test_name = Contacts.new({:full_name => "Ricky Bobby"})
+      test_name = Contacts.new({:full_name => "Ricky Bobby", :job => "race car driver", :company => "Wonderbread racing"})
       expect(test_name.first_name()).to(eq("Ricky"))
     end
   end
 
   describe('#last_name') do
     it("displays the contacts last name") do
-      test_name = Contacts.new({:full_name => "Ricky Bobby"})
+      test_name = Contacts.new({:full_name => "Ricky Bobby", :job => "race car driver", :company => "Wonderbread racing"})
       expect(test_name.last_name()).to(eq("Bobby"))
+    end
+  end
+
+  describe('#job') do
+    it("displays the contacts job") do
+      test_name = Contacts.new({:full_name => "Ricky Bobby", :job => "race car driver", :company => "Wonderbread racing"})
+      expect(test_name.job()).to(eq("Race Car Driver"))
+    end
+  end
+
+  describe('#company') do
+    it("displays the contacts company") do
+      test_name = Contacts.new({:full_name => "ricky bobby", :job => "race car driver", :company => "Wonderbread racing"})
+      expect(test_name.company()).to(eq("Wonderbread Racing"))
     end
   end
 end
